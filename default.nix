609c1027787a272f2d0d78796b76185cfeb4a960
benjamin.bajorat <benjamin.bajorat@mayflower.de>
2020-12-28 16:16:38 +0100

    adding pushercoins functionality

---
+++ default.nix
@@ -0,0 +1,11 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
+  env = buildEnv { name = name; paths = buildInputs; };
+  buildInputs = [
+    nodejs-14_x
+    yarn
+    nodePackages.typescript
+  ];
+}
+
